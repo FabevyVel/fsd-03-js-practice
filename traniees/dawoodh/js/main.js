b06d150f28bfcec5ffda0db3c89db8bc9fc96c0a
shakdawoodh <muhamedrafiq123@gmail.com>
2021-06-05 16:31:18 +0530

    Adding js File (Const)

--- traniees/dawoodh/js/main.js
+++ traniees/dawoodh/js/main.js
@@ -1,6 +1,9 @@
 // Random Number
+const min = 1, max = 7;
+PlayerScore = 0;
+machineScore = 0;
 
-function rollTheDice (min, max) {
+function rollTheDice () {
     let randomNumber = Math.random() * (max - min) + min;
     randomNumber = Math.floor(randomNumber);
     return randomNumber;

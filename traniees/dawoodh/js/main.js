0507e4775d00831a1733962e37f7405bd05eabe8
shakdawoodh <muhamedrafiq123@gmail.com>
2021-06-05 21:58:18 +0530

    Adding Playerscore funtion.

--- traniees/dawoodh/js/main.js
+++ traniees/dawoodh/js/main.js
@@ -1,10 +1,20 @@
 // Random Number
-const min = 1, max = 7;
-PlayerScore = 0;
+
+let gamingInstruction,playerColorCode,machineColorCode,playerscore,machineScore;
+
+const min = 1 , max = 7;
+playerscore = 0;
 machineScore = 0;
 
 function rollTheDice () {
     let randomNumber = Math.random() * (max - min) + min;
     randomNumber = Math.floor(randomNumber);
     return randomNumber;
+}
+
+function updateplayerscore(){
+    let dicescore = rollTheDice ();
+    console.log('playerscore: '  + playerscore +       'dicescore:' + dicescore)
+    playerscore = playerscore + dicescore;
+    return playerscore;
 }

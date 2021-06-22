0e03c9a12b3c0d8f998e019f74ce15c7eeafd009
srinath.s <targaryensj@gmail.com>
2021-06-22 06:30:31 +0530

    update new file.

--- traniees/sri.js/practice-jquery/js/main.js
+++ traniees/sri.js/practice-jquery/js/main.js
@@ -5,3 +5,24 @@ $('button').click(function(){
 
 //select => Excute
 
+$('.accordion-head').click(function(){
+   
+  
+  if($(this).hasClass('active')){
+  
+  }else{
+   $('.accordion-body').hide();
+   $('.accordion-head').removeClass('active')
+   $('.accordion-body').removeClass('active')
+  }
+
+
+
+  $('.accordion-body').hide();
+
+  $(this).toggleClass('active');
+  $(this).siblings('.accordion-body').toggleClass('active');
+  $(this).siblings('.accordion-body').toggle();
+
+
+});

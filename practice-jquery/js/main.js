05a4484e1e9cdec45cf0534e67074fb2943ea920
FabevyVel <vel@fabevy.com>
2021-06-22 12:40:20 +0530

    add accordion.

--- practice-jquery/js/main.js
+++ practice-jquery/js/main.js
@@ -13,3 +13,30 @@ $('button').click(function(){
 // Which element & What event (click, double click) ?
 // What action to be performed ?
 // On which element the action to be performed ?
+
+
+
+// --------
+
+// Which element => $('.accordion-head')
+// What event => .click(function(){   })
+// What action to be performed => Toggle (Show / Hide)
+// On which element the action to be performed => $('.accordion-body')
+
+$('.accordion-head').click(function(){
+
+    if($(this).hasClass('active')){
+
+    }else{
+        $('.accordion-body').hide();
+        $('.accordion-head').removeClass('active');
+        $('.accordion-body').removeClass('active');
+    }
+
+    $(this).toggleClass('active');
+    $(this).siblings('.accordion-body').toggleClass('active');
+
+    $(this).siblings('.accordion-body').toggle();
+
+
+});
